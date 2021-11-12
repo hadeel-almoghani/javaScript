a3c9556aab651b6f0517f9b0e59666090c77d7f6
hadeel-almoghani <hadolmm132@gmail.com>
2021-11-12 17:02:23 +0200

    Java script

---
+++ homework/script.js
@@ -0,0 +1,29 @@
+function createTable()
+{
+rn = window.prompt("Input number of rows", 1);
+cn = window.prompt("Input number of columns",1);
+  
+ for(var r=0;r<parseInt(rn,10);r++)
+  {
+   var x=document.getElementById('myTable').insertRow(r);
+   for(var c=0;c<parseInt(cn,10);c++)  
+    {
+     var y=  x.insertCell(c);
+     y.innerHTML="Row-"+r+" Column-"+c; 
+    }
+   }
+}
+
+
+function volume_sphere()
+ {
+  var volume;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+  return false;
+ } 
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
